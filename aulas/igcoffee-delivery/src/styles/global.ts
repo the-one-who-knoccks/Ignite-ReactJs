8dc72178fa80734d7e1a8866c03862231718f852
the-one-who-knoccks <thiagoalves.devp>
2022-10-06 22:38:06 -0300

    :art: Added themes

--- aulas/igcoffee-delivery/src/styles/global.ts
+++ aulas/igcoffee-delivery/src/styles/global.ts
@@ -1,20 +1,35 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
+ * { 
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+ }
 
-  body {
-    background: #333;
-    color: #fff;
-  }
+ :focus {
+  outline: 0;
+  
+ }
 
-  body, input, textarea, button {
-    font-family: "Roboto", sans-serif;
-    font-weight: 400;
+ body {
+    background: ${({ theme }) => theme.colors['base-background']};
+    color: ${({ theme }) => theme.colors['base-text']};
     -webkit-font-smoothing: antialiased;
-    }
+  }
+  body, input, textarea, button {
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-weight: 400;
+    font-size: ${({ theme }) => theme.textSizes['text-regular-m']}
+  }
+
+
+button {
+  cursor: pointer;
+}
+
+a {
+  text-decoration: none;
+}
+
 `
