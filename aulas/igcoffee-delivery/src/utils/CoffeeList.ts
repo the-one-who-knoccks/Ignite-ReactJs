30216f93b71414503f46878c265369eb7da5f57a
the-one-who-knoccks <thiagoalves.devp>
2022-10-07 20:43:32 -0300

    :art: Updated JSON file

--- aulas/igcoffee-delivery/src/utils/CoffeeList.ts
+++ aulas/igcoffee-delivery/src/utils/CoffeeList.ts
@@ -1,158 +1,118 @@
-// Images //
-import AmericanExpressImage from './../assets/coffees/american-express.png'
-import TradicionalExpressImage from './../assets/coffees/tradicional-express.png'
-import ArabianCoffeeImage from './../assets/coffees/arabian-coffee.png'
-import CapuccinoImage from './../assets/coffees/capuccino.png'
-import CoffeeWithMilkImage from './../assets/coffees/coffee-with-milk.png'
-import CreamyExpressImage from './../assets/coffees/creamy-express.png'
-import CubanCoffeeImage from './../assets/coffees/cuban-coffee.png'
-import HawaiianCoffeeImage from './../assets/coffees/hawaiian-coffee.png'
-import HotCoffeeImage from './../assets/coffees/hot-chocolate.png'
-import IceExpressImage from './../assets/coffees/ice-express.png'
-import IrishCoffeeImage from './../assets/coffees/irish-coffee.png'
-import LatteImage from './../assets/coffees/latte.png'
-import MacchiatoImage from './../assets/coffees/macchiato.png'
-import MocaccinoImage from './../assets/coffees/mocaccino.png'
-
-export interface Coffee {
-  id: number
-  name: string
-  description: string
-  price: number
-  tags: string[]
-  image: string
-  isAvaliable?: boolean
-}
-
-export const coffeeList: Coffee[] = [
+export const coffees = [
   {
     id: 1,
+    tags: ['tradicional'],
     name: 'Expresso Tradicional',
     description: 'O tradicional café feito com água quente e grãos moídos',
+    photo: 'tradicional.png',
     price: 9.9,
-    tags: ['tradicional'],
-    image: TradicionalExpressImage,
-    isAvaliable: true,
   },
   {
     id: 2,
+    tags: ['tradicional'],
     name: 'Expresso Americano',
     description: 'Expresso diluído, menos intenso que o tradicional',
+    photo: 'americano.png',
     price: 8.9,
-    tags: ['tradicional'],
-    image: AmericanExpressImage,
-    isAvaliable: true,
   },
   {
     id: 3,
+    tags: ['tradicional'],
     name: 'Expresso Cremoso',
     description: 'Café expresso tradicional com espuma cremosa',
+    photo: 'cremoso.png',
     price: 12.9,
-    tags: ['tradicional'],
-    image: CreamyExpressImage,
-    isAvaliable: true,
   },
   {
     id: 4,
+    tags: ['tradicional', 'gelado'],
     name: 'Expresso Gelado',
     description: 'Bebida preparada com café expresso e cubos de gelo',
+    photo: 'gelado.png',
     price: 10.9,
-    tags: ['tradicional', 'gelado'],
-    image: IceExpressImage,
-    isAvaliable: true,
   },
   {
     id: 5,
+    tags: ['tradicional', 'com leite'],
     name: 'Café com Leite',
     description: 'Meio a meio de expresso tradicional com leite vaporizado',
+    photo: 'leite.png',
     price: 10.9,
-    tags: ['tradicional', 'com leite'],
-    image: CoffeeWithMilkImage,
-    isAvaliable: true,
   },
   {
     id: 6,
+    tags: ['tradicional', 'com leite'],
     name: 'Latte',
     description:
       'Uma dose de café expresso com o dobro de leite e espuma cremosa',
-    price: 13.9,
-    tags: ['tradicional', 'com leite'],
-    image: LatteImage,
-    isAvaliable: true,
+    photo: 'latte.png',
+    price: 12.9,
   },
   {
     id: 7,
+    tags: ['tradicional', 'com leite'],
     name: 'Capuccino',
     description:
       'Bebida com canela feita de doses iguais de café, leite e espuma',
-    price: 15.9,
-    tags: ['tradicional', 'com leite'],
-    image: CapuccinoImage,
-    isAvaliable: true,
+    photo: 'capuccino.png',
+    price: 13.9,
   },
   {
     id: 8,
+    tags: ['tradicional', 'com leite'],
     name: 'Macchiato',
     description:
       'Café expresso misturado com um pouco de leite quente e espuma',
-    price: 14.9,
-    tags: ['tradicional', 'com leite'],
-    image: MacchiatoImage,
-    isAvaliable: true,
+    photo: 'macchiato.png',
+    price: 17.9,
   },
   {
     id: 9,
+    tags: ['tradicional', 'com leite'],
     name: 'Mocaccino',
     description: 'Café expresso com calda de chocolate, pouco leite e espuma',
+    photo: 'mocaccino.png',
     price: 17.9,
-    tags: ['tradicional', 'com leite'],
-    image: MocaccinoImage,
-    isAvaliable: true,
-  },
-  {
-    id: 9,
-    name: 'Chocolate Quente',
-    description: 'Bebida feita com chocolate dissolvido no leite quente e café',
-    price: 10.9,
-    tags: ['especial', 'com leite'],
-    image: HotCoffeeImage,
-    isAvaliable: true,
   },
   {
     id: 10,
-    name: 'Cubano',
-    description:
-      'Drink gelado de café expresso com rum, creme de leite e hortelã',
-    price: 13.9,
-    tags: ['especial', 'alcoólico', 'gelado'],
-    image: CubanCoffeeImage,
-    isAvaliable: true,
+    tags: ['especial', 'com leite'],
+    name: 'Chocolate Quente',
+    description: 'Bebida feita com chocolate dissolvido no leite quente e café',
+    photo: 'chocolate.png',
+    price: 10.9,
   },
   {
     id: 11,
-    name: 'Havaiano',
-    description: 'Bebida adocicada preparada com café e leite de coco',
-    price: 19.9,
-    tags: ['especial'],
-    image: HawaiianCoffeeImage,
-    isAvaliable: true,
+    tags: ['especial', 'alcoólico', 'gelado'],
+    name: 'Cubano',
+    description:
+      'Drink gelado de café expresso com rum, creme de leite e hortelã',
+    photo: 'cubano.png',
+    price: 9.9,
   },
   {
     id: 12,
-    name: 'Árabe',
-    description: 'Bebida preparada com grãos de café árabe e especiarias',
-    price: 25.9,
     tags: ['especial'],
-    image: ArabianCoffeeImage,
-    isAvaliable: true,
+    name: 'Havaiano',
+    description: 'Bebida adocicada preparada com café e leite de coco',
+    photo: 'havaiano.png',
+    price: 10.9,
   },
   {
     id: 13,
+    tags: ['especial'],
+    name: 'Árabe',
+    description: 'Bebida preparada com grãos de café árabe e especiarias',
+    photo: 'arabe.png',
+    price: 13.9,
+  },
+  {
+    id: 14,
+    tags: ['especial', 'alcoólico'],
     name: 'Irlandês',
     description: 'Bebida a base de café, uísque irlandês, açúcar e chantilly',
-    price: 22.9,
-    tags: ['especial', 'alcoólico'],
-    image: IrishCoffeeImage,
-    isAvaliable: true,
+    photo: 'irlandes.png',
+    price: 20.9,
   },
 ]
