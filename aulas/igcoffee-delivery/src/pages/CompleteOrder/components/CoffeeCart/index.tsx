6cc82433522abf6bc8782b243677a9384c4206e7
the-one-who-knoccks <thiagoalves.devp>
2022-10-09 13:46:22 -0300

    :sparkles: Added context features

--- aulas/igcoffee-delivery/src/pages/CompleteOrder/components/CoffeeCart/index.tsx
+++ aulas/igcoffee-delivery/src/pages/CompleteOrder/components/CoffeeCart/index.tsx
@@ -1,26 +1,54 @@
 import { ActionsContainer, CoffeeCartContainer, RemoveButton } from './styles'
-import ExpressoImg from '../../../../../public/coffees/americano.png'
 import { RegularText } from '../../../../components/Typography'
 import { AddInput } from '../../../../components/AddInput'
 import { Trash } from 'phosphor-react'
+import { CartItem } from '../../../../context/CartContext'
+import { formatMoney } from '../../../../utils/formatMoney'
+import { useCart } from '../../../../hooks/useCart'
 
-export function CoffeeCart() {
+interface CoffeeCartCardProps {
+  coffee: CartItem
+}
+
+export function CoffeeCart({ coffee }: CoffeeCartCardProps) {
+  const { updateCartAmount, removeCartItem } = useCart()
+
+  function handleIncrease() {
+    updateCartAmount(coffee.id, 'increase')
+  }
+
+  function handleDecrease() {
+    updateCartAmount(coffee.id, 'decrease')
+  }
+
+  function hadnleRemoveItem() {
+    removeCartItem(coffee.id)
+  }
+
+  const coffeeTotal = coffee.price * coffee.amount
+  const formattedPrice = formatMoney(coffeeTotal)
+
   return (
     <CoffeeCartContainer>
       <div>
-        <img src={ExpressoImg} alt="" />
+        <img src={`/coffees/${coffee.photo}`} alt="" />
         <div>
-          <RegularText color="subtitle">Expresso Tradicional</RegularText>
+          <RegularText color="subtitle">{coffee.name}</RegularText>
           <ActionsContainer>
-            <AddInput size="small" />
-            <RemoveButton>
+            <AddInput
+              size="small"
+              amount={coffee.amount}
+              onDecrease={handleDecrease}
+              onIncrease={handleIncrease}
+            />
+            <RemoveButton onClick={hadnleRemoveItem}>
               <Trash size={16} />
               REMOVER
             </RemoveButton>
           </ActionsContainer>
         </div>
       </div>
-      <p>RS 9,90</p>
+      <p>RS {formattedPrice}</p>
     </CoffeeCartContainer>
   )
 }
--- aulas/igcoffee-delivery/src/pages/CompleteOrder/components/ConfirmationSection/index.tsx
+++ aulas/igcoffee-delivery/src/pages/CompleteOrder/components/ConfirmationSection/index.tsx
@@ -1,28 +1,40 @@
 import { Button } from '../../../../components/Button'
 import { RegularText } from '../../../../components/Typography'
+import { useCart } from '../../../../hooks/useCart'
+import { formatMoney } from '../../../../utils/formatMoney'
 import { ConfirmationSectionContainer } from './styles'
 
+const DELIVERY_PRICE = 3.5
+
 export function ConfirmationSection() {
+  const { CartItemsTotal, cartAmount } = useCart()
+
+  const cartTotal = DELIVERY_PRICE + CartItemsTotal
+
+  const formattedTotalItems = formatMoney(CartItemsTotal)
+  const formattedTotal = formatMoney(cartTotal)
+  const formatDeliveryPrice = formatMoney(DELIVERY_PRICE)
+
   return (
     <ConfirmationSectionContainer>
       <div>
         <RegularText size="s">Total de itens</RegularText>
-        <RegularText size="s">R$ 9,90</RegularText>
+        <RegularText size="s">R$ {formattedTotalItems}</RegularText>
       </div>
 
       <div>
         <RegularText size="s">Entrega</RegularText>
-        <RegularText size="s">R$ 3,50</RegularText>
+        <RegularText size="s">R$ {formatDeliveryPrice}</RegularText>
       </div>
       <div>
         <RegularText weight="700" color="subtitle" size="l">
           Total
         </RegularText>
         <RegularText weight="700" color="subtitle" size="l">
-          R$ 29,90
+          R$ {formattedTotal}
         </RegularText>
       </div>
-      <Button text="Confirmar pedido" />
+      <Button text="Confirmar pedido" disabled={cartAmount <= 0} />
     </ConfirmationSectionContainer>
   )
 }
