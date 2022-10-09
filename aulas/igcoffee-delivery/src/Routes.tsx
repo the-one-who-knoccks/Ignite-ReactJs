abd51a1f6e36855d07bc0aebc39546952ae10e77
the-one-who-knoccks <thiagoalves.devp>
2022-10-08 21:40:02 -0300

    :sparkles: Added route

--- aulas/igcoffee-delivery/src/Routes.tsx
+++ aulas/igcoffee-delivery/src/Routes.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import { DefaultLayout } from './layouts/Default/DefaultLayout'
 import { CompleteOrderPage } from './pages/CompleteOrder'
 import { Home } from './pages/Home'
+import { ConfirmedOrder } from './pages/ConfirmedOrder'
 
 export function Router() {
   return (
@@ -9,6 +10,7 @@ export function Router() {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/complete-order" element={<CompleteOrderPage />} />
+        <Route path="confirmed-order" element={<ConfirmedOrder />} />
       </Route>
     </Routes>
   )
