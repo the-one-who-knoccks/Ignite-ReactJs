4e827a5cf1f4b0d545448381483a527153496a44
the-one-who-knoccks <thiagoalves.devp>
2022-10-10 10:16:21 -0300

    :zap: Change Input component

--- aulas/igcoffee-delivery/src/components/Input/styles.ts
+++ aulas/igcoffee-delivery/src/components/Input/styles.ts
@@ -1,16 +1,54 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const InputStyleContainer = styled.input`
+interface InputStyleContainerProps {
+  hasError: boolean
+}
+
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  gap: 0.3rem;
+  position: relative;
+
+  > p {
+    color: ${({ theme }) => theme.colors['base-error']};
+  }
+`
+
+export const InputStyleContainer = styled.div<InputStyleContainerProps>`
   height: 2.625rem;
   border-radius: 4px;
   border: 1px solid ${({ theme }) => theme.colors['base-button']};
   background: ${({ theme }) => theme.colors['base-input']};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   transition: 0.4s;
+  overflow: hidden;
 
-  &:focus {
+  &:focus-within {
     border-color: ${({ theme }) => theme.colors['brand-yellow-dark']};
   }
 
+  ${({ theme, hasError }) =>
+    hasError &&
+    css`
+      border-color: ${({ theme }) => theme.colors['base-error']};
+    `}
+`
+
+export const RightPlaceholder = styled.p`
+  font-size: 0.75rem;
+  margin-right: 0.75rem;
+  font-style: italic;
+  color: ${({ theme }) => theme.colors['base-label']};
+`
+export const StyledInput = styled.input`
+  flex: 1;
+  height: 100%;
+  background: none;
+  border: none;
   color: ${({ theme }) => theme.colors['base-text']};
   font-size: 0.75rem;
   padding: 0 0.75rem;
