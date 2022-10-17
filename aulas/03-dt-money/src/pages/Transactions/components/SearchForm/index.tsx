b622dc718a2d7d7f16cc1d3cd21323db803be5c7
the-one-who-knoccks <thiagoalves.devp>
2022-10-17 13:24:40 -0300

    :sparkles: Make search form usable

--- aulas/03-dt-money/src/pages/Transactions/components/SearchForm/index.tsx
+++ aulas/03-dt-money/src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,11 +1,44 @@
+import { useForm } from 'react-hook-form'
 import { SearchFormContainer } from './styles'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { MagnifyingGlass } from 'phosphor-react'
+import { useContext } from 'react'
+import { TransactionsContext } from '../../../../context/TransactionsContext'
+
+const SearchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof SearchFormSchema>
 
 export function SearchForm() {
-  return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Busque uma transação" />
+  const { fetchTransactions } = useContext(TransactionsContext)
 
-      <button type="submit">Buscar</button>
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(SearchFormSchema),
+  })
+
+  async function handleSearchForm(data: SearchFormInputs) {
+    await fetchTransactions(data.query)
+  }
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchForm)}>
+      <input
+        {...register('query')}
+        type="text"
+        placeholder="Busque uma transação"
+      />
+
+      <button type="submit" disabled={isSubmitting}>
+        <MagnifyingGlass size={20} />
+        Buscar
+      </button>
     </SearchFormContainer>
   )
 }
--- aulas/03-dt-money/src/pages/Transactions/index.tsx
+++ aulas/03-dt-money/src/pages/Transactions/index.tsx
@@ -1,5 +1,8 @@
+import { useContext } from 'react'
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import { TransactionsContext } from '../../context/TransactionsContext'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
 import { SearchForm } from './components/SearchForm'
 import {
   HighlightPrice,
@@ -8,6 +11,8 @@ import {
 } from './styles'
 
 export function Transactions() {
+  const { transactions } = useContext(TransactionsContext)
+
   return (
     <div>
       <Header />
@@ -17,22 +22,23 @@ export function Transactions() {
         <SearchForm />
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td>
-                <HighlightPrice variant="income">R$ 12.000,00</HighlightPrice>
-              </td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
-            <tr>
-              <td width="50%">Hamburger</td>
-              <td>
-                <HighlightPrice variant="outcome">- R$ 59,00</HighlightPrice>
-              </td>
-              <td>Alimentação</td>
-              <td>13/04/2022</td>
-            </tr>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <HighlightPrice variant={transaction.type}>
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
+                    </HighlightPrice>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>
+                    {dateFormatter.format(new Date(transaction.createdAt))}
+                  </td>
+                </tr>
+              )
+            })}
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
