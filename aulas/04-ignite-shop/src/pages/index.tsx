3db246b91179eb8e0291697c0cf500419bcbd7b1
the-one-who-knoccks <thiagoalves.devp>
2022-11-23 12:19:43 -0300

    :tada: Initial commit

---
+++ aulas/04-ignite-shop/src/pages/index.tsx
@@ -0,0 +1,27 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+  cursor: 'pointer',
+
+  span: {
+    fontWeight: 'bold',
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)'
+  }
+})
+
+export default function Home() {
+  return (
+    <Button>
+      <span>Teste</span>
+      Enviar
+      </Button>
+
+  )
+}
