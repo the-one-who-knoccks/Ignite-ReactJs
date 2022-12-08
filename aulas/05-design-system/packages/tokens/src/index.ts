a9909df221f3cbcd95841f1ed48e4efc1a67981c
the-one-who-knoccks <thiagoalves.devp>
2022-12-07 23:12:37 -0300

    :tada: Created design system monorepo

---
+++ aulas/05-design-system/packages/tokens/src/index.ts
@@ -0,0 +1,7 @@
+export * from './colors'
+export * from './font-sizes'
+export * from './fonts'
+export * from './line-heights'
+export * from './radii'
+export * from './radii'
+export * from './space'
