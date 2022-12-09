11821e9577f7b223c0c55a2a21d4d54dbfd43f06
the-one-who-knoccks <thiagoalves.devp>
2022-12-09 19:07:40 -0300

    :zap: Created turborepo

--- aulas/05-design-system/packages/docs/src/stories/Button.stories.tsx
+++ aulas/05-design-system/packages/docs/src/stories/Button.stories.tsx
@@ -3,7 +3,7 @@ import { StoryObj, Meta } from '@storybook/react'
 import { Button, ButtonProps } from '@ignite-ui/react'
 
 export default {
-  title: 'Button',
+  title: 'Form/Button',
   component: Button,
 
   args: {
--- aulas/05-design-system/packages/tokens/src/index.ts
+++ aulas/05-design-system/packages/tokens/src/index.ts
@@ -1,6 +1,7 @@
 export * from './colors'
 export * from './font-sizes'
 export * from './fonts'
+export * from './font-weights'
 export * from './line-heights'
 export * from './radii'
 export * from './radii'
