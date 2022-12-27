fe19420fed40ab033fcafff8adabcf37ea9ab4b4
the-one-who-knoccks <thiagoalves.devp>
2022-12-27 14:54:23 -0300

    :tada: Begin project and add some structural components

---
+++ aulas/ignite-call/src/pages/home/index.tsx
@@ -0,0 +1,32 @@
+import { Heading, Text } from '@ignite-ui/react'
+import Image from 'next/image'
+import { Container, Hero, Preview } from './styles'
+import PreviewImg from '../../assets/app-preview.png'
+import { ClaimUsernameForm } from './components/ClaimUsernameForm'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading as="h1" size="4xl">
+          Agendamento descomplicado
+        </Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre
+        </Text>
+        <ClaimUsernameForm />
+      </Hero>
+
+      <Preview>
+        <Image
+          src={PreviewImg}
+          alt="Calendário da aplicação"
+          height={400}
+          quality={100}
+          priority
+        />
+      </Preview>
+    </Container>
+  )
+}
